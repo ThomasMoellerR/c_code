ce9968b858d2687a63ff6df05d17ba8452daff26
ThomasMoellerR <thomas.moeller.r@gmail.com>
2019-05-12 11:12:42 +0200

    Update ser1.h

--- ser1.h
+++ ser1.h
@@ -1,60 +1,62 @@
 
 /******************************************************************************
-
-   File                :     ser1.h
-   Version             :     11
-   
-   Version Temp-File   :     2
-   
+*  File           :  ser1.h
+*  Version        :  1
+*  Last Changes   :  2019-05-12 11:06:06
 ******************************************************************************/
 
 #ifndef __SER1_H
 #define __SER1_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /******************************************************************************
-   Include-Files
-******************************************************************************/   
+*  Include Files
+******************************************************************************/
 
 #include "typ.h"
 
 /******************************************************************************
-   Global Constants
-******************************************************************************/   
+*  Global Constants
+******************************************************************************/
 
-//#define SER1_SENDING_MODE_POLLING				// Define it for mode polling
-#define SER1_SENDING_MODE_DMA					// Define it for mode DMA
+//#define SER1_SENDING_MODE_POLLING             // Define it for mode polling
+#define SER1_SENDING_MODE_DMA                   // Define it for mode DMA
 
-#define XOR_CHECKSUM							// Define it for XOR checksum
+#define XOR_CHECKSUM                            // Define it for XOR checksum
 
-#define SER1_STX						0x65	// Start sign "A"
-#define SER1_ETX						0x0D	// End sign (Carriage Return)
+#define SER1_STX                        0x65    // Start sign "A"
+#define SER1_ETX                        0x0D    // End sign (Carriage Return)
 
-#define SER1_SNDBUF_QUELEN 				256 	// Length of send queue (Polling mode)
-#define SER1_RECBUF_QUELEN 				256 	// Length of rec queue
+#define SER1_SNDBUF_QUELEN              256     // Length of send queue (Polling mode)
+#define SER1_RECBUF_QUELEN              256     // Length of rec queue
 
-#define SER1_SNDBUF_LEN					1024 	// Length of buffer for frame with payload to send
-#define SER1_RECBUF_LEN					1024	// Length of payload buffer
-
-/******************************************************************************
-   Global Type-Definitions
-******************************************************************************/   
+#define SER1_SNDBUF_LEN                 1024    // Length of buffer for frame with payload to send
+#define SER1_RECBUF_LEN                 1024    // Length of payload buffer
 
 /******************************************************************************
-   Global Variables
-******************************************************************************/   
+*  Global Type Definitions
+******************************************************************************/
+
+/******************************************************************************
+*  Global Variables
+******************************************************************************/
 
 // Sending
-TUINT8 SER1_u8SendPollingRunning; 				// Indicates the polling state if polling is used. For DMA transfer not relevant
-TUINT8 SER1_au8SndBuf[SER1_SNDBUF_LEN]; 		// Frame with payload to send
-TUINT8 SER1_TransmitFrameLength;				// For DMA transfer
+extern TUINT8 SER1_u8SendPollingRunning; 				// Indicates the polling state if polling is used. For DMA transfer not relevant
+extern TUINT8 SER1_au8SndBuf[SER1_SNDBUF_LEN]; 		// Frame with payload to send
+extern TUINT8 SER1_TransmitFrameLength;				// For DMA transfer
 
 // Receiving
-TUINT8 SER1_au8RecBuf[SER1_RECBUF_LEN]; 		// Extracted payload of received frame
-TUINT8 SER1_u8ReceivedPayloadLength; 					// Number of data in received payload
+extern TUINT8 SER1_au8RecBuf[SER1_RECBUF_LEN]; 		// Extracted payload of received frame
+extern TUINT8 SER1_u8ReceivedPayloadLength; 					// Number of data in received payload
+
 
 /******************************************************************************
-   Global Function-Prototypes
-******************************************************************************/   
+*  Global Function Prototypes
+******************************************************************************/
 
 void SER1_Ini (void);
 
@@ -75,6 +77,19 @@ void SER1_RecBuf_Put (TUINT8 u8Data); 						// Rec Queue
 TUINT8 SER1_u8RecBuf_Get (void); 							// Rec Queue
 TUINT8 SER1_u8RecBuf_Empty (void); 							// Rec Queue
 
+/******************************************************************************
+*  Changes        :  
+*                    
+*                    
+*                    
+******************************************************************************/
+
+#ifdef __cplusplus
+}
 #endif
 
+#endif /* __SER1_H */
 
+/******************************************************************************
+*  END OF FILE    :  ser1.h
+******************************************************************************/
